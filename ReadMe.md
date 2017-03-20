f937d2e72b667604288c2b07b0623b69dbd9c1c7
jonathancmitchell <jmitchell1991@gmail.com>
2017-03-19 18:28:10 -0700

    [feature] - adds test.py model.py makeVideo.py files for tester and adds sequenceToVideo inside NvidiaModel

--- ReadMe.md
+++ ReadMe.md
@@ -16,3 +16,33 @@ It was unclear exactly how it was being implemented so I reached out to the auth
 
 You can drag the train_vo.prototxt to this link: http://ethereon.github.io/netscope/#/editor
 to see the network model and all its intricacies
+
+3) DeepFlow: Large displacement optical flow with deep matching [link](http://www.cv-foundation.org/openaccess/content_iccv_2013/papers/Weinzaepfel_DeepFlow_Large_Displacement_2013_ICCV_paper.pdf)
+* I considered using DeepFlow but I found out about it literally the day before the project was due
+
+4) Car Speed estimation using Visual Odometry
+
+Extra:
+TODO: [Keras Model Visualization] (https://keras.io/visualization/)
+
+Step1: Run VideoToDatasetAcquisition in an ipython notebook. Just shift click your way through it. This will create a driving.csv file and an IMG folder with all the images. I did this so we can work with image paths instead of the actual images, and to make life easier
+
+Step2: Run NvidiaModel to build the Nvidia model
+* At the end of the file you will create a predict folder that holds the images with predictions overlayed on top of them. Then if you shift click through it you will create a video and then you can watch that video in the ipython notebook.
+
+
+
+### TO TEST
+# Step 1: Run ./setupstuff.sh 
+* This will create the necessary folders (driving_test.csv, test_IMG, test_predict)
+`./setupstuff.sh`
+
+# Step 2: python test.py
+* Go into test.py and specify the paths to video file, and the paths to the data (json) file
+* This will log out the MSE
+`python test.py`
+
+# step 3: makeVideo.py
+`python makeVideo.py`
+* This will create a video to see how well the prediction works with the test set
+* Requires moviepy
---
+++ test.py
@@ -0,0 +1,143 @@
+
+import cv2
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+import json
+import csv
+
+
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import matplotlib.gridspec as gridspec
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+
+
+# change these depending on your file names / paths
+TEST_GROUND_TRUTH_JSON_PATH = './data/drive.json' # change this to the test ground truth
+VIDEO_PATH = './data/drive.mp4' # change this to the test video
+TEST_IMG_PATH = './test/test_IMG/'
+DRIVE_TEST_CSV_PATH = './test/driving_test.csv'
+TEST_PREDICT_PATH = './test/test_predict/'
+
+WEIGHTS = 'model-weights-F5.h5'
+EVAL_SAMPLE_SIZE = 100 # Number of samples to evaluate to compute MSE
+
+with open(TEST_GROUND_TRUTH_JSON_PATH) as json_data:
+    ground_truth = json.load(json_data)
+    # json_data.close()
+
+# write image_paths, time, and speed to .csv file
+with open(DRIVE_TEST_CSV_PATH, 'w') as csvfile:
+    fieldnames = ['image_path', 'time', 'speed']
+    writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+    writer.writeheader()
+    
+
+    vidcap = cv2.VideoCapture(VIDEO_PATH)
+
+    for idx, item in enumerate(ground_truth):
+        
+        
+        
+        # set video capture to specific time frame
+        # multiply time given in ground truth by 1000 to convert to milliseconds
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, item[0] * 1000)
+        
+        # read in the image
+        success, image = vidcap.read()
+    
+        if success:
+            image_path = os.path.join(TEST_IMG_PATH, str(item[0]) + '.jpg')
+            
+            # save image to IMG folder
+            cv2.imwrite(image_path, image)
+            
+            # write row to driving.csv
+            writer.writerow({'image_path': image_path, 
+                     'time':item[0],
+                     'speed':item[1],
+                    })
+print('done writing to driving_test.csv and test_IMG folder')
+
+
+### Preprocessing helpers
+def preprocess_image(image):
+    image_cropped = image[100:440, :-90] # -> (380, 550, 3)
+    image = cv2.resize(image_cropped, (220, 66), interpolation = cv2.INTER_AREA)
+    return image
+
+
+def preprocess_image_valid_from_path(image_path, speed):
+    img = cv2.imread(image_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = preprocess_image(img)
+    return img, speed
+
+
+
+from model import nvidia_model
+from keras.models import Sequential
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.core import Activation, Dropout, Flatten, Dense, Lambda
+from keras.layers import ELU
+from keras.optimizers import Adam
+tf.python.control_flow_ops = tf
+N_img_height = 66
+N_img_width = 220
+N_img_channels = 3
+
+
+model = nvidia_model()
+model.load_weights(WEIGHTS)
+
+
+### MODEL EVALUATION
+samples = EVAL_SAMPLE_SIZE
+errors = []
+batch = []
+labels = []
+data = pd.read_csv(DRIVE_TEST_CSV_PATH)
+indices = [np.random.randint(1, len(data) - 1) for i in range(samples)]
+
+for idx in indices:
+    row_now = data.iloc[[idx]].reset_index()
+    row_prev = data.iloc[[idx - 1]].reset_index()
+    row_next = data.iloc[[idx + 1]].reset_index()
+
+    # Find the 3 respective times to determine frame order (current -> next)
+
+    time_now = row_now['time'].values[0]
+    time_prev = row_prev['time'].values[0]
+    time_next = row_next['time'].values[0]
+
+    if time_now - time_prev > 0 and 0.0000001 < time_now - time_prev < 0.58: # 0.578111 is highest diff i have seen
+        # in this case row_prev is x1 and row_now is x2
+        row1 = row_prev
+        row2 = row_now
+
+    elif time_next - time_now > 0 and 0.0000001 < time_next - time_now < 0.58:
+        # in this case row_now is x1 and row_next is x2
+        row1 = row_now
+        row2 = row_next
+
+    x1, y1 = preprocess_image_valid_from_path(row1['image_path'].values[0], row1['speed'].values[0])
+    x2, y2 = preprocess_image_valid_from_path(row2['image_path'].values[0], row2['speed'].values[0])
+    img_diff = x1 - x2
+    img_diff_reshaped = img_diff.reshape(1, img_diff.shape[0], img_diff.shape[1], img_diff.shape[2])
+    prediction = model.predict(img_diff_reshaped)
+    errors.append(np.square(abs(prediction - y2)))
+
+MSE = np.mean(errors)
+print('MSE: ', MSE)
+
+
+# save prediction to pickle file
+
+
+
