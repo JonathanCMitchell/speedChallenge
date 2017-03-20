b7bfcbf3fb70a30327b0e3c451bdf3af52fa3307
jonathancmitchell <jmitchell1991@gmail.com>
2017-03-19 19:09:08 -0700

    [refactor] - uploads movie.mp4 and changed weight analysis to model-weight-F4 in test.py

--- test.py
+++ test.py
@@ -24,7 +24,8 @@ TEST_IMG_PATH = './test/test_IMG/'
 DRIVE_TEST_CSV_PATH = './test/driving_test.csv'
 TEST_PREDICT_PATH = './test/test_predict/'
 
-WEIGHTS = 'model-weights-F5.h5'
+# WEIGHTS = 'model-weights-F5.h5' # this one is less overfit but performs 10% worse
+WEIGHTS = 'model-weights-F4.h5' # this one performs better and hopefully isn't overfit
 EVAL_SAMPLE_SIZE = 100 # Number of samples to evaluate to compute MSE
 
 with open(TEST_GROUND_TRUTH_JSON_PATH) as json_data:
